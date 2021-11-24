e1e5c51609955065ecb33ef7c79574f7f5021013
Vaibhav Gupta <guptavaibhav432@gmail.com>
2021-11-25 00:47:27 +0530

    Make Footer and Menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,61 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
-import { Navbar } from "./components";
-
+import {
+    Navbar,
+    Exchanges,
+    HomePage,
+    Cryptocurrencies,
+    CryptoDetails,
+    News,
+} from "./components";
+import "./App.css";
 const App = () => {
     return (
         <div className="app">
             <div className="navbar">
                 <Navbar />
             </div>
-            <div className="main"></div>
-            <div className="footer"></div>
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path="/" element={<HomePage />} />
+                            <Route
+                                exact
+                                path="/exchange"
+                                element={<Exchanges />}
+                            />
+                            <Route
+                                exact
+                                path="/cryptocurrencies"
+                                element={<Cryptocurrencies />}
+                            />
+                            <Route
+                                exact
+                                path="/crypto/:coinId"
+                                element={<CryptoDetails />}
+                            />
+                            <Route exact path="/news" element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className="footer">
+                <Typography.Title
+                    level={5}
+                    style={{ color: "white", textAlign: "center" }}
+                >
+                    CryptHacker <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchange">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
+            </div>
+            
         </div>
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,8 +20,21 @@ const Navbar = () => {
                 <Typography.Title level={2} className="logo">
                     <Link to="/">CryptHacker</Link>
                 </Typography.Title>
-                {/* <Button className="menu-control-container"></Button> */}
             </div>
+            <Menu theme="dark">
+                <Menu.Item key="1" icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item key="2" icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item key="3" icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchange">Exchange</Link>
+                </Menu.Item>
+                <Menu.Item key="4" icon={<BulbOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     );
 };
